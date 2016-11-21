24425b6cceb914e616b2639f7c714c326375ab58
Damien SIEST <damiensiest-etudes@outlook.fr>
2016-11-21 15:12:23 +0100

    yolobis

--- IHM/TD2/grapher/src/grapher/ui/Main.java
+++ IHM/TD2/grapher/src/grapher/ui/Main.java
@@ -1,6 +1,9 @@
 package grapher.ui;
 
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Vector;
 
 import javax.swing.JFrame;
 import javax.swing.JList;
@@ -9,23 +12,23 @@ import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 
-public class Main extends JFrame {
+public class Main extends JFrame implements MouseListener {
 	Main(String title, String[] expressions) {
 		super(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);	
-		String[] func={};
+		Vector func= new Vector();
 		Grapher grapher = new Grapher();
-		int i=0;
 		for(String expression : expressions) {
 			grapher.add(expression);
-			func[i]=expression;
-			i++;
+			func.addElement(expression);
 		}
 		JList list=new JList(func);
 		JSplitPane splitPane=new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,list,grapher);
 		add(splitPane);
 		splitPane.setOneTouchExpandable(true);
 		splitPane.setDividerLocation(150);
+		this.addMouseListener(list);
+	
 
 		//Provide minimum sizes for the two components in the split pane
 		Dimension minimumSize = new Dimension(100, 50);
@@ -43,5 +46,35 @@ public class Main extends JFrame {
 				new Main("grapher", expressions).setVisible(true); 
 			}
 		});
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
