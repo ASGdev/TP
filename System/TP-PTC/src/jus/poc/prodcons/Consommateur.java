c11628f81c65d234a77b9fdafae92ee7f21118f2
Shadsa <zehava-bol@live.fr>
2016-12-10 16:09:25 +0100

    Final Struct

---
+++ System/TP-PTC/src/jus/poc/prodcons/Consommateur.java
@@ -0,0 +1,11 @@
+package jus.poc.prodcons;
+
+public class Consommateur extends Acteur implements _Consommateur {
+
+	public Consommateur(int type, Observateur observateur, int moyenneTempsDeTraitement,
+			int deviationTempsDeTraitement) {
+		super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		// TODO Auto-generated constructor stub
+	}
+
+}
