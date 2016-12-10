557d8ef2b225311e51f73cc04e1e93eb37b6c24c
Damien SIEST <damiensiest-etudes@outlook.fr>
2016-12-10 16:57:28 +0100

    Test

--- System/TP-PTC/src/jus/poc/test/Test.java
+++ System/TP-PTC/src/jus/poc/test/Test.java
@@ -1,7 +1,11 @@
 package jus.poc.test;
 
+import java.util.Random;
 
 public class Test {
 
-	public int main(){return 0;};
+	public int main(){
+		public final Aleatoire randomizeur = new Aleatoire(2,10);
+		
+	};
 }
