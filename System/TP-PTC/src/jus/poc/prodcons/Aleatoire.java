92e45544bdc5198b9cadb8082448a726ac6712dd
Damien SIEST <damiensiest-etudes@outlook.fr>
2016-12-11 10:43:11 +0100

    Aleatoire achieved

--- System/TP-PTC/src/jus/poc/prodcons/Aleatoire.java
+++ System/TP-PTC/src/jus/poc/prodcons/Aleatoire.java
@@ -3,34 +3,35 @@ package jus.poc.prodcons;
 import java.util.Random;
 
 public class Aleatoire {
-	protected int borneInf;
-	protected int borneSup;
-	protected int moyenne;
-	protected int deviation;
-	protected Random var = new Random();
+	protected static int borneInf;
+	protected static int borneSup;
+	protected static  int moyenne;
+	protected static  int deviation;
+	protected static Random var = new Random();
 	
 	Aleatoire(int moyenne, int deviation){
-			this.moyenne=moyenne;
-			this.deviation=deviation;
-		};
-	
-	int next(){
-		double nextGaussianValue = var.nextGaussian() * deviation + moyenne;
-		return 0;
+		Aleatoire.moyenne=moyenne;
+		Aleatoire.deviation=deviation;
+	};
+
+static int next(){
+	double nextGaussianValue = var.nextGaussian() * deviation + moyenne;
+	return (int)nextGaussianValue;
+}
+
+static int valeur(int moyenne, int deviation){
+	int plage = borneSup-borneInf;
+	int nextValeurValue=var.nextInt(plage)+borneInf;
+	return nextValeurValue;
+}
+
+static int[] valeurs(int size,int moyenne, int deviation){
+	int[] tab = new int[size];
+	for (int i = 0; i < tab.length; i++) {
+		tab[i] = next();
 	}
-	
-	int valeur(int moyenne, int deviation){
-		int nextValeurValue=var.nextInt(borneSup-borneInf)+borneInf;
-		return 0;
-	}
-	
-	int[] valeurs(int size,int moyenne, int deviation){
-		int[] tab = new int[size];
-		for (int i = 0; i < tab.length; i++) {
-			tab[i] = this.next();
-		}
-		return tab;
-	}
-	
-	
+	return tab;
+}
+
+
 }
