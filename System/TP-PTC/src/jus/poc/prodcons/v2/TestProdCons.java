320abbd3006192d525ad86e6efce22a5c2787a3d
Shadsa <zehava-bol@live.fr>
2016-12-13 23:57:01 +0100

    LAst

---
+++ System/TP-PTC/src/jus/poc/prodcons/v2/TestProdCons.java
@@ -0,0 +1,57 @@
+package jus.poc.prodcons.v2;
+
+import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Map;
+import java.util.Properties;
+
+public class TestProdCons extends Simulateur {
+
+	public TestProdCons(Observateur observateur){super(observateur);}
+	
+	protected void run() throws Exception{
+		while(true){
+			System.out.println("1");
+		}
+		// le corps de votre programme principalz
+		
+		//PASSAGE DES OPTIONS
+		
+		//INIT MESSAGE 
+		
+		//INIT DES THREADS
+		
+		//SIMULATION
+		//ProdCons ProdCons = new ProdCons(3,3,this.observateur);
+		
+	}
+	
+	
+	public static void main(String[] args){
+		new TestProdCons(new Observateur()).start();//n'hérite pas de thread, ca sert a rien...
+		int buffer = 6;
+		Observateur observateur = new Observateur();
+		ProdCons ProdCons = new ProdCons(3,3, buffer,observateur);
+		
+	}
+	
+	
+	
+	
+	protected void initOption(String file) throws InvalidPropertiesFormatException, IOException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		Properties properties = new Properties();
+		properties.loadFromXML(ClassLoader.getSystemResourceAsStream(file));
+		String key;
+		int value;
+		Class<?> thisOne = getClass();
+		for(Map.Entry<Object,Object> entry : properties.entrySet()) {
+			key = (String)entry.getKey();
+			value = Integer.parseInt((String)entry.getValue());
+			thisOne.getDeclaredField(key).set(this,value);
+		}
+		options = properties;
+	}
+
+
+}
+
