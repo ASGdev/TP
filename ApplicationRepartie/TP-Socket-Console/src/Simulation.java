7a8ecda2327b1745b83eea3f51bcd1d0bcca5e2d
Damien SIEST <damiensiest-etudes@outlook.fr>
2017-01-22 16:36:52 +0100

    Create of the simulation

---
+++ ApplicationRepartie/TP-Socket-Console/src/Simulation.java
@@ -0,0 +1,17 @@
+
+public class Simulation {
+	int Threadpool = 10;
+	
+	public Simulation(int n) {
+		this.Threadpool = n;
+	}
+	
+	public static void main(int[] args) {
+		// TODO Auto-generated method stub
+		Simulation simulation = new Simulation((args[0]));
+		for (int i = 0; i < simulation.Threadpool; i++) {
+			Client client = new Client();
+			client.mainTCP();
+		}
+	}
+}
