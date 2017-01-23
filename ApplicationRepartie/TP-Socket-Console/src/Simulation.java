60fa6b2c0e429230158dfaa00d7650beb0ebb30c
Damien SIEST <damiensiest-etudes@outlook.fr>
2017-01-23 09:06:22 +0100

    Kapoooom

--- ApplicationRepartie/TP-Socket-Console/src/Simulation.java
+++ ApplicationRepartie/TP-Socket-Console/src/Simulation.java
@@ -9,10 +9,11 @@ public class Simulation {
 	public static void main(int[] args) {
 		// TODO Auto-generated method stub
 		Simulation simulation = new Simulation((args[0]));
+		Client client = new Client();
 		for (int i = 0; i < simulation.Threadpool; i++) {
-			Client client = new Client();
+			
 			ClientThread clientthread = new ClientThread(client);
-			client.mainTCP();
+			clientthread.mainTCP();
 		}
 	}
 }
