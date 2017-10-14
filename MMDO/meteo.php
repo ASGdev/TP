79ba4853b2e147f332ecbbc4a5fab3cbced8b1e3
TitouanLarnicol <larnicol.titouan@orange.fr>
2017-10-14 13:12:21 +0200

    Add files via upload
    5 questions finis. Il en reste 3 !

--- MMDO/meteo.php
+++ MMDO/meteo.php
@@ -14,17 +14,29 @@
 
 <?php 
 	include("station.php");
+	
+	
 ?>
 
+<table style="width:100%">
+		
+<?php
 
+	affichage_tableaux($info_station);
+	
+?>
+</table>
 	
 
-	<table style="width:100%">
-	  	<?php
-		affichage_synthetique($liste_station);
-	  	?>
+<table style="width:100%">
+<?php
+if (!$info_station) {
+	affichage_synthetique($liste_station);
+}
+
+?>
 	  	
-	</table>
+</table>
 
 </body>
 
--- MMDO/station.php
+++ MMDO/station.php
@@ -6,6 +6,7 @@
 </head>
 <body>
 <?php
+include("test.php"); 
 
 
 $indice_station=[33,34,72,73,98,100,110,118,120,131];
@@ -57,14 +58,7 @@ function fill_data_station($liste_station){
 	return($info_station);
 }
 $info_station=fill_data_station($liste_station); 
-?>
-<table style="width:100%">
-		
-<?php
-	affichage_tableaux($info_station);
-?>
-</table>
-<?php	
+	
 	
 //AFFICHAGE EN Dynamique/statique
 function affichage_tableaux($info_station){
@@ -86,66 +80,8 @@ function affichage_tableaux($info_station){
    	}
    	echo "</tr>";	
 }
-// TRES SALE ET LONG... TROUVE LE MIN ET MAX DE TEMP, PLUIE ET HUMIDITE
-function minmax($liste_station){
-	//ON TROUVE LES MAX ET MIN
-	
-	$liste_parametres=['temperature','pluie','humidite'];
-	$min_humidite=$liste_station[0]['humidite'];$max_humidite=$liste_station[0]['humidite'];$indice_humidite_min=0;$indice_humidite_max=0;
-	$min_pluie=$liste_station[0]['pluie'];$max_pluie=$liste_station[0]['pluie'];$indice_pluie_min=0;$indice_pluie_max=0;
-	$min_temperature=$liste_station[0]['temperature'];$max_temperature=$liste_station[0]['temperature'];$indice_temperature_min=0;$indice_temperature_max=0;
-	for ($i=1; $i <10 ; $i++) { 
-		//HUMIDITE
-		$j=0;
-		while ($j<3) {
-			
-			if($i!=7 && $liste_parametres[$j]=='humidite' && ($liste_station[$i]['humidite']<$min_humidite || $liste_station[$i]['humidite']>$max_humidite)){
 
 
-			if ($liste_station[$i]['humidite']<$min_humidite ) {
-				$min_humidite=$liste_station[$i]['humidite'];
-				$indice_humidite_min=$i;
-				
-			}
-			elseif($liste_station[$i]['humidite']>$max_humidite){
-				$max_humidite=$liste_station[$i]['humidite'];
-				$indice_humidite_max=$i;
-			}
-			else{}
-				
-		}
-		elseif($i!=7 && $liste_parametres[$j]=='temperature' && ($liste_station[$i]['temperature']<$min_temperature || $liste_station[$i]['temperature']>$max_temperature)){
-			if ($liste_station[$i]['temperature']<$min_temperature ) {
-				$min_temperature=$liste_station[$i]['temperature'];
-				$indice_temperature_min=$i;
-			}
-
-			elseif($liste_station[$i]['temperature']>$max_temperature){
-				$max_temperature=$liste_station[$i]['temperature'];
-				$indice_temperature_max=$i;
-
-			}
-			else{}
-		}
-		elseif($i!=7 && $liste_parametres[$j]=='pluie' && ($liste_station[$i]['pluie']<$min_pluie || $liste_station[$i]['pluie']>$max_pluie)){
-			if ($liste_station[$i]['pluie']<$min_pluie ) {
-				$min_pluie=$liste_station[$i]['pluie'];
-				$indice_pluie_min=$i;
-			}
-			elseif($liste_station[$i]['pluie']>$max_pluie){
-				$max_pluie=$liste_station[$i]['pluie'];
-				$indice_pluie_max=$i;
-
-			}
-			else{}
-		}
-		else{}  
-		$j++;
-		}	
-	}
-	$tab_min_max=[$indice_pluie_min,$indice_pluie_max,$indice_temperature_min,$indice_temperature_max,$indice_humidite_min,$indice_humidite_max];
-	return($tab_min_max);		
-}
 
 //AFFICHAGE TABLEAUX SYNTHETIQUE
 function affichage_synthetique($liste_station){
@@ -153,10 +89,14 @@ function affichage_synthetique($liste_station){
 	$liste_parametres=['station','latitude','longitude','valide','date','heure','temperature','pluie','humidite'];
 	//affiche les paramètres(station,latitude...)
 	for ($k=0; $k <9 ; $k++) {  
+		
 		echo "<td>";
 		print_r($liste_parametres[$k]);
 		echo "</td>";
 	}	
+	echo "<td>";
+	echo "MAP";;
+	echo "</td>";
 	//Il y a 10 stations à parcourir
 	for ($i=0; $i <10 ; $i++) {
 		echo "<tr>"; 
@@ -172,44 +112,20 @@ function affichage_synthetique($liste_station){
 				echo "</td>";
 			}
 			else{
-				
-				if ($parametre=='humidite') {
-					if ($tab[4]==$i) {
-						echo "<td";
-						echo " style=\"border: 1px solid red;\"";
-						echo ">";
-						print_r($liste_station[$i][$parametre]);
-						echo "</td>";
-					}
-					elseif ($tab[5]==$i) {
-						echo "<td";
-						echo " style=\"border: 1px solid red;\"";
-						echo ">";
-						print_r($liste_station[$i][$parametre]);
-						echo "</td>";
-					}
-					else{
-						echo "<td>";
-					print_r($liste_station[$i][$parametre]);
-					echo "</td>";
-					}
-				} 
-				else{
-					echo "<td>";
-					print_r($liste_station[$i][$parametre]);
-					echo "</td>";
-				}		  	
-			} 							
-		}	
+				//COLORIE LE MIN/MAX
+				coloriage($parametre,$liste_station,$i,$tab);			  	
+			}
+									
+		}
+	echo "<td>";
+	affiche_map($liste_station,$i);	
+	echo "</td>";
 	echo "</tr>";
 	}
 
-	
-		
-	}
-
-
+}
 ?>
+
 </body>
 </html>
 
