b260ba3fe19f1c893f54a623040dcc49cdf3cca2
Shadsa <zehava-bol@live.fr>
2017-11-11 15:35:10 +0100

    Test page home MMDO

--- MMDO/AndroidEnv/ProjetMMDO/src/pages/home/home.ts
+++ MMDO/AndroidEnv/ProjetMMDO/src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+interface Results{
+  title : string,
+  author:string,
+  date : number,
+  image: string
+}
+
+interface searchResult{
+  researchlist : Results[]
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  toto : string = "toto";
+
+  Film : Results ={
+    title :"Truc",
+    author : "Bidule",
+    date :15,
+    image :"TrucNul"
+  };
+
+  result : searchResult = {researchlist : []};
+  searchInput : string = "";
 
   constructor(public navCtrl: NavController) {
+    for (let index = 0; index < 3; index++) {
+      this.result.researchlist[index]=this.Film;
+      
+    }
+  }
 
+  onInput($event){
+    console.log(this.searchInput);    
+    this.searchInput = "troll";
   }
 
 }
