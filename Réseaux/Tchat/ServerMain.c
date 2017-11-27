5c90ff9f665e04ea00e5336f04343cbd7275dbcb
Shadsa <zehava-bol@live.fr>
2017-11-27 14:46:40 +0100

    save

--- Réseaux/Tchat/ServerMain.c
+++ Réseaux/Tchat/ServerMain.c
@@ -20,6 +20,28 @@ typedef struct in_addr IN_ADDR;
 
 #else /* sinon vous êtes sur une plateforme non supportée */
 
-#error not defined for this platform
-
 #endif
+
+int main(int argc, char* argv[]){
+
+}
+
+static void init(void)
+{
+#ifdef WIN32
+    WSADATA wsa;
+    int err = WSAStartup(MAKEWORD(2, 2), &wsa);
+    if(err < 0)
+    {
+        puts("WSAStartup failed !");
+        exit(EXIT_FAILURE);
+    }
+#endif
+}
+
+static void end(void)
+{
+#ifdef WIN32
+    WSACleanup();
+#endif
+}
