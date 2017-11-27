1e4fd02e60600ce80bca0fb00fe34f48e381c7cb
Shadsa <zehava-bol@live.fr>
2017-11-27 15:01:53 +0100

    .h

--- Réseaux/Tchat/ServerMain.c
+++ Réseaux/Tchat/ServerMain.c
@@ -1,3 +1,6 @@
+/*Une partie des entête, inclusion et certaine fonction proviennent du site http://broux.developpez.com/articles/c/sockets/
+afin de garantir un dévellopement portable car notre groupe avais des machines linux et windows.*/
+
 #ifdef WIN32 /* si vous êtes sous Windows */
 
 #include <winsock2.h> 
@@ -17,16 +20,22 @@ typedef int SOCKET;
 typedef struct sockaddr_in SOCKADDR_IN;
 typedef struct sockaddr SOCKADDR;
 typedef struct in_addr IN_ADDR;
-
 #else /* sinon vous êtes sur une plateforme non supportée */
-
 #endif
+int PORT = 9999;
+
 
 int main(int argc, char* argv[]){
-    init();
+    init(void);
 
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock == INVALID_SOCKET)
+    {
+        perror("socket()");
+        exit(errno);
+    }
 
-    end();
+    end(void);
 }
 
 static void init(void)
