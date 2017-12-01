4f3878cbfa42054e8def2914b333af61681e1fd8
Shadsa <zehava-bol@live.fr>
2017-12-01 15:00:25 +0100

    recup des data

--- MMDO/AndroidEnv/ProjetMMDO/src/pages/home/home.ts
+++ MMDO/AndroidEnv/ProjetMMDO/src/pages/home/home.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DetailsPage } from '../details/details';
+import {HttpClientModule} from '@angular/common/http';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/pluck';
-import { HttpClient } from '@angular/common/http/src/client';
 
 interface Result {
   title: string;
@@ -32,35 +32,27 @@ const result: Result[]=[{
 export class HomePage {
   url_base = 'https://api.themoviedb.org/3/search/movie?api_key=ebb02613ce5a2ae58fde00f4db95a9c1&query=';
   results: string[];
+  searchInput: string= "";
   // Inject HttpClient into your component or service.
-  constructor(private http: HttpClient) {}
-  
-   ngOnInit(): void {
-     // Make the HTTP request:
-        this.http.get('/api/items').subscribe(data => {
-       // Read the result field from the JSON response.
-       this.results = data['results'];
-     });
-   }
-  }
+
   searchresult:Result=result[1];
   items=result;
   pushPage: any;
-  constructor(public navCtrl: NavController) {
-  }
+  constructor(public navCtrl: NavController, private http: HttpClientModule) {}
   //changement de page
   private showDetails(){
     this.navCtrl.push(DetailsPage)
   }
 
-  private searchEngine(){
+  private searchEngine(req){
     // Make the HTTP request:
-    this.http.get('/api/items').subscribe(data => {
+    this.http.get(this.url_base + encodeURI(req)).subscribe(data => {
       // Read the result field from the JSON response.
       this.results = data['results'];
+    });
   }
 
-  searchInput: string= "";
+
   onInput($event){
   if(!this.searchInput || this.searchInput!='Film'){
       this.items=[];
