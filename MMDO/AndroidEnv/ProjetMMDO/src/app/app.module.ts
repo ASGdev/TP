203a2af853c6264102d106c5b08b63362d3db271
Shadsa <zehava-bol@live.fr>
2017-12-01 14:45:14 +0100

    Titoo puull MMDO + début requete toimemetusais

--- MMDO/AndroidEnv/ProjetMMDO/src/app/app.module.ts
+++ MMDO/AndroidEnv/ProjetMMDO/src/app/app.module.ts
@@ -3,26 +3,28 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import {HttpClientModule} from '@angular/common/http'
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { DetailsPage } from '../pages/details/details';
 
 @NgModule({
-  declarations: [
+  declarations: [ 
     MyApp,
-    DetailsPage,
-    HomePage
+    HomePage,
+    DetailsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    DetailsPage,
-    HomePage
+    HomePage,
+    DetailsPage
   ],
   providers: [
     StatusBar,
--- MMDO/AndroidEnv/ProjetMMDO/src/pages/home/home.ts
+++ MMDO/AndroidEnv/ProjetMMDO/src/pages/home/home.ts
@@ -1,44 +1,74 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DetailsPage } from '../details/details';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/pluck';
+import { HttpClient } from '@angular/common/http/src/client';
 
-interface Results{
-  title : string,
-  author:string,
-  date : number,
-  image: string
+interface Result {
+  title: string;
+  author: string;
+  date: number;
+  image: string;
 }
 
-interface searchResult{
-  researchlist : Results[]
-}
-
+const result: Result[]=[{  
+  title:"Film1",
+  author:"ezf",
+  date:15,
+  image:"ddd"
+},{  
+  title:"Film2",
+  author:"ezf",
+  date:15,
+  image:"ddd"
+}]
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
+  
 })
+
 export class HomePage {
-  toto : string = "toto";
-
-  Film : Results ={
-    title :"Truc",
-    author : "Bidule",
-    date :15,
-    image :"TrucNul"
-  };
-
-  result : searchResult = {researchlist : []};
-  searchInput : string = "";
-
+  url_base = 'https://api.themoviedb.org/3/search/movie?api_key=ebb02613ce5a2ae58fde00f4db95a9c1&query=';
+  results: string[];
+  // Inject HttpClient into your component or service.
+  constructor(private http: HttpClient) {}
+  
+   ngOnInit(): void {
+     // Make the HTTP request:
+        this.http.get('/api/items').subscribe(data => {
+       // Read the result field from the JSON response.
+       this.results = data['results'];
+     });
+   }
+  }
+  searchresult:Result=result[1];
+  items=result;
+  pushPage: any;
   constructor(public navCtrl: NavController) {
-    for (let index = 0; index < 3; index++) {
-      this.result.researchlist[index]=this.Film;
-      
-    }
+  }
+  //changement de page
+  private showDetails(){
+    this.navCtrl.push(DetailsPage)
   }
 
+  private searchEngine(){
+    // Make the HTTP request:
+    this.http.get('/api/items').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data['results'];
+  }
+
+  searchInput: string= "";
   onInput($event){
-    console.log(this.searchInput);    
-    this.searchInput = "troll";
-  }
-
+  if(!this.searchInput || this.searchInput!='Film'){
+      this.items=[];
+      return;
+    }
+  else{
+      this.items=result;
+    }
+  };
 }
+
