673fa7101a5b5788f85532b3c95c6f2e7fae16fa
Shadsa <zehava-bol@live.fr>
2017-12-01 15:18:55 +0100

    YOLO C'EST NAWAK LE TS

--- MMDO/AndroidEnv/ProjetMMDO/src/pages/home/home.ts
+++ MMDO/AndroidEnv/ProjetMMDO/src/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DetailsPage } from '../details/details';
-import {HttpClientModule} from '@angular/common/http';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/pluck';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { HttpParams } from '@angular/common/http/src/params';
+
 
 interface Result {
   title: string;
@@ -30,7 +31,8 @@ const result: Result[]=[{
 })
 
 export class HomePage {
-  url_base = 'https://api.themoviedb.org/3/search/movie?api_key=ebb02613ce5a2ae58fde00f4db95a9c1&query=';
+  private api_key: string = 'ebb02613ce5a2ae58fde00f4db95a9c1';
+  private url_base: string = 'https://api.themoviedb.org/3/search/movie';
   results: string[];
   searchInput: string= "";
   // Inject HttpClient into your component or service.
@@ -38,28 +40,27 @@ export class HomePage {
   searchresult:Result=result[1];
   items=result;
   pushPage: any;
-  constructor(public navCtrl: NavController, private http: HttpClientModule) {}
+  constructor(public navCtrl: NavController, private http: HttpClient) {}
   //changement de page
   private showDetails(){
     this.navCtrl.push(DetailsPage)
   }
 
-  private searchEngine(req){
+  private searchEngine():Observable<Result[]>{
     // Make the HTTP request:
-    this.http.get(this.url_base + encodeURI(req)).subscribe(data => {
-      // Read the result field from the JSON response.
-      this.results = data['results'];
-    });
+    return this.http.get<Result[]>(this.url_base,{
+      params: new HttpParams().set('api_key',this.api_key).set('query',this.searchInput)
+    }).pluck('results');
   }
 
 
   onInput($event){
-  if(!this.searchInput || this.searchInput!='Film'){
-      this.items=[];
+  if(!this.searchInput){
+      this.results=[];
       return;
     }
   else{
-      this.items=result;
+     this.results=this.searchEngine();
     }
   };
 }
