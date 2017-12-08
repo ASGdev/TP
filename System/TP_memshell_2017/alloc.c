00baa2376df9fe2df7a8e7434e3d2a0f9769601d
Unknown <larnicol.titouan@orange.fr>
2017-12-08 14:47:15 +0100

    Auto stash before merge of "master" and "origin/master"

--- System/TP_memshell_2017/alloc.c
+++ System/TP_memshell_2017/alloc.c
@@ -7,7 +7,7 @@ fb* firstZone;
 
 
 void mem_init(){
-    //Initialise ou réinitialise la liste des zones libres.
+    //Initialise liste des zones libres avec la taille du tableau initial
     firstZone = (fb*) mem_heap;
     firstZone -> size = HEAP_SIZE;
     firstZone -> next = NULL;
@@ -17,6 +17,9 @@ void mem_init(){
 void *mem_alloc(size_t size){
     fb *zone_courante = firstZone;
     //Pointeur de pointeur sur le bloc précédent. Il prend donc une adresse et pas une fb.
+    //Il nous servira pour accéder à l'élément suivant ( le bloc à allouer) et à retenir l'adresse du 
+    //bloc courant suivant
+    fb *bloc_null = NULL;
     fb **zone_memory = &firstZone;
 
     if(firstZone == NULL){
@@ -39,13 +42,14 @@ void *mem_alloc(size_t size){
             zone_courante = zone_courante->next;
         }
     }
-    //taille du bloc parfaite
+    //taille du bloc parfaite du coup : probleme si on alloue la taille entière du tableaux ça allou bien
+    //mais a -ton du coup un descripteur de bloc  ? 
     if(zone_courante->size == size){
         *zone_memory = zone_courante->next;
     }
     //size du bloc trop importante
     else{
-        //on crée le nouveau bloc qui va fusionner
+        //on crée le nouveau bloc qui va fusionner ((sans char* j'ai un 0 en trop))
         fb *newBlock = (fb*) ((char *)zone_courante + size);
         newBlock->size = zone_courante->size - size;
         newBlock->next = zone_courante->next;
@@ -58,21 +62,22 @@ void *mem_alloc(size_t size){
 void mem_free(void *zone, size_t size){
     fb *zone_courante = firstZone;
     fb *newBlock = (fb*) zone;
-    //même pb avec la taille que pour alloc
+    //même pb avec la taille que pour alloc : on re utilise la formule (utilisation schema pour m'aider)
     if( size % sizeof(fb) ){
         size = size + sizeof(fb) - ( size % sizeof(fb) );
     }
     newBlock->size = size;
-    //Si tout était complet ou que le bloc à libérer est avant le premier bloc libre
+    //Si tout était complet
+    //ou bloc à libérer est avant le premier bloc libre : On chaine donc le bloc à allouer au firstBloc !
     if( zone_courante == NULL || zone_courante > (fb*) zone){
         firstZone = newBlock; // on le chaine au début
         newBlock->next = zone_courante;  //on chaine le next à la deuxième zone libre
     }
-    else{
+    else{ //on cherche la zone en comparant les @ -> Parcours basique
         while(zone_courante->next != NULL && zone_courante->next < (fb*) zone){
             zone_courante = zone_courante->next; 
         }
-        //Les deux blocs doivent être fusionnés
+        //Les deux blocs doivent être fusionnés si l'@+la size du courant est égale à l'@ donnée 
         if((char*) zone_courante + zone_courante->size == (char*) zone){
             zone_courante->size = zone_courante->size + size;
             newBlock = zone_courante;
