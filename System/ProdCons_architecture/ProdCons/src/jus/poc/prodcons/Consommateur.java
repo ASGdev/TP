da00a280d67abb5c8e478afe51b4253bd3f03fda
Shadsa <zehava-bol@live.fr>
2017-12-15 15:51:49 +0100

    Conso & Prod done, a test et a corriger

--- System/ProdCons_architecture/ProdCons/src/jus/poc/prodcons/Consommateur.java
+++ System/ProdCons_architecture/ProdCons/src/jus/poc/prodcons/Consommateur.java
@@ -2,7 +2,8 @@ package jus.poc.prodcons;
 
 public class Consommateur extends Acteur implements _Consommateur {
 	int nbMessage =0;
-
+	ProdCons tampon;
+	
 	protected Consommateur(int type, Observateur observateur, int moyenneTempsDeTraitement,
 			int deviationTempsDeTraitement,int nbMessage) throws ControlException {
 		super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
@@ -39,5 +40,41 @@ public class Consommateur extends Acteur implements _Consommateur {
 			treatment(getMessage());
 		}
 	}
+	
+
+	private MessageX getMessage(){
+		Message tamp = null;
+		System.out.println("Demande d'accee de "+this.getName()+"N°"+this.identification()+"");
+		try {
+			tamp = tampon.get(this);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		nbMessage -=1;
+		System.out.println("Tampon libre:"+tampon.taille()+" avec le retrait de "+this.getName()+"N°"+this.identification()+", reste "+nbMessage+"a traiter");
+		System.out.println("Sortie d'accee de "+this.getName()+"N°"+this.identification()+"");
+		return (MessageX) tamp;		
+	}
+
+	private void treatment(Message m){
+		int delay = Aleatoire.valeur(moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		if(m!=null){
+			try {
+				Thread.sleep(delay);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			System.out.println("Traitement de consommation du thread "+this.getName()+"N°"+this.identification()+", reste "+nbMessage+"a traiter");
+		}else{
+			System.out.println("Rien a faire pour thread "+this.getName()+"N°"+this.identification());
+			try {
+				Thread.sleep(Aleatoire.valeur(moyenneTempsDeTraitement, deviationTempsDeTraitement));
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}	
+	}
+
 
 }
--- System/ProdCons_architecture/ProdCons/src/jus/poc/prodcons/Producteur.java
+++ System/ProdCons_architecture/ProdCons/src/jus/poc/prodcons/Producteur.java
@@ -36,38 +36,37 @@ public class Producteur extends Acteur implements _Producteur {
 	@Override
 	public void run() {
 		while(this.nbMessage > 0){			
-			treatment(getMessage());
+			addMessage(production());
 		}
 	}
-	
-	private MessageX getMessage(){
-		Message tamp = null;
+
+	private void addMessage(Message m){
 		System.out.println("Demande d'accee de "+this.getName()+"N°"+this.identification()+"");
-		tamp = tampon.get(this);
-		nbMessage -=1;
-		System.out.println("Tampon libre:"+tampon.taille()+" avec le retrait de "+this.getName()+"N°"+this.identification()+", reste "+nbMessage+"a traiter");
-		System.out.println("Sortie d'accee de "+this.getName()+"N°"+this.identification()+"");
-		return tamp;		
-	}
-	
-	private void treatment(Message m){
-		int delay = Aleatoire.valeur(moyenneTempsDeTraitement, deviationTempsDeTraitement);
-		if(m!=null){
-			try {
-				Thread.sleep(delay);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}			System.out.println("Traitement de consommation du thread "+this.getName()+"N°"+this.identification()+", reste "+nbMessage+"a traiter");
-		}else{
-			System.out.println("Rien a faire pour thread "+this.getName()+"N°"+this.identification());
-			try {
-				Thread.sleep(Aleatoire.valeur(moyenneTempsDeTraitement, deviationTempsDeTraitement));
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		try {
+			tampon.put(this,m);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		}	
+		nbMessage -=1;
+		System.out.println("Tampon libre:"+tampon.taille()+" avec l'ajout de "+this.getName()+"N°"+this.identification()+", reste "+nbMessage+"a traiter");
+		System.out.println("Sortie d'accee de "+this.getName()+"N°"+this.identification()+"");	
+
+	}
+
+
+
+	private Message production(){
+		MessageX m = new MessageX(this.nbMessage);
+		try {
+			Thread.sleep(Aleatoire.valeur(moyenneTempsDeTraitement, deviationTempsDeTraitement));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Traitement de production du thread "+this.getName()+"N°"+this.identification());
+
+		return m;
+
 	}
 
 }
